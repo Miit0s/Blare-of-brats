fc68a6dbab3264d6f3106d0017e34fd6cddbc007
Ewen R <106588588+Miit0s@users.noreply.github.com>
2026-03-23 15:16:31 +0100

    Variable to stop sooner the item slide

--- Scripts/item.gd
+++ Scripts/item.gd
@@ -20,6 +20,10 @@ class_name Item
 		object_texture_size = new_value
 		_ready()
 
+##The minimal speed the item should have. When the speed is under this threshold, the speed is set to zero
+var minimal_speed: float = 50
+var has_been_throw: bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var collision_shape: CircleShape2D = collision_shape_2d.shape
@@ -28,10 +32,14 @@ func _ready() -> void:
 	sprite_2d.texture = object_texture
 	sprite_2d.scale = Vector2(object_texture_size, object_texture_size)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+func _physics_process(_delta: float) -> void:
+	if not has_been_throw: return
+	
+	if linear_velocity.length() < minimal_speed:
+		linear_velocity = Vector2.ZERO
+		has_been_throw = false
 
 func throw(direction: Vector2):
 	apply_central_impulse(direction.normalized() * throw_force)
+	await get_tree().create_timer(0.1).timeout
+	has_been_throw = true
