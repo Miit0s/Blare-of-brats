5fff35c13ec72a693ddc441267a4bb8516b7075e
Ewen R <106588588+Miit0s@users.noreply.github.com>
2026-04-16 15:31:58 +0200

    Main menu can be controll with controller (not the option page for the moment)

--- Scripts/UI/controller_slot.gd
+++ Scripts/UI/controller_slot.gd
@@ -14,19 +14,21 @@ var is_ready: bool = false
 
 @export var hold_duration: float = 0.75
 var player_is_holding_ready_key: bool
-var _hold_duration: float = 0.0
+var _hold_time: float = 0.0
+
+signal player_his_ready()
+signal player_no_more_ready()
 
 func _process(delta: float) -> void:
 	if player_is_holding_ready_key and not is_ready:
-		_hold_duration = move_toward(_hold_duration, 1.0, delta / hold_duration)
+		_hold_time = move_toward(_hold_time, 1.0, delta / hold_duration)
 		
-		if _hold_duration >= 1: 
-			is_ready = true
-			ready_texture.show()
+		if _hold_time >= 1: 
+			player_ready()
 	elif not is_ready:
-		_hold_duration = move_toward(_hold_duration, 0.0, delta / hold_duration)
+		_hold_time = move_toward(_hold_time, 0.0, delta / hold_duration)
 	
-	radial_progress_bar.material.set_shader_parameter("progress", _hold_duration)
+	radial_progress_bar.material.set_shader_parameter("progress", _hold_time)
 
 func set_player_id(new_id: int):
 	_player_id = new_id
@@ -46,9 +48,19 @@ func remove_player():
 func get_player_id() -> int:
 	return _player_id
 
+func player_ready():
+	is_ready = true
+	ready_texture.show()
+	ready_container.hide()
+	radial_progress_bar.material.set_shader_parameter("progress", 0)
+	_hold_time = 0
+	player_his_ready.emit()
+
 func back():
 	if is_ready:
 		ready_texture.hide()
+		ready_container.show()
 		is_ready = false
+		player_no_more_ready.emit()
 	elif not is_slot_available:
 		remove_player()
--- Scripts/UI/main_menu.gd
+++ Scripts/UI/main_menu.gd
@@ -1,11 +1,18 @@
 extends Control
 
+@onready var play_button: Button = $MainButton/Play
+@onready var options_button: Button = $MainButton/Options
+@onready var credits_button: Button = $MainButton/Credits
+@onready var exit_button: Button = $MainButton/Exit
+
 @onready var quit_prompt: Control = $QuitPrompt
 @onready var credits: Control = $Credits
 @onready var options: Control = $Options
 
 @export var game_start_scene: PackedScene
 
+func _ready() -> void:
+	play_button.grab_focus()
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_packed(game_start_scene)
@@ -21,3 +28,13 @@ func _on_credits_pressed() -> void:
 
 func _on_exit_pressed() -> void:
 	quit_prompt.show()
+
+
+func _on_quit_prompt_visibility_changed() -> void:
+	if not quit_prompt.visible:
+		exit_button.grab_focus()
+
+
+func _on_credits_visibility_changed() -> void:
+	if not credits.visible:
+		credits_button.grab_focus()
