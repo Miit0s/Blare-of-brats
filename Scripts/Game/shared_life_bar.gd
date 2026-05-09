c2d19c4bd90b3421060bf655bff8578c2a276990
Ewen R <106588588+Miit0s@users.noreply.github.com>
2026-05-09 15:06:25 +0200

    life bar now correctly trigger game end

--- Scripts/Game/shared_life_bar.gd
+++ Scripts/Game/shared_life_bar.gd
@@ -19,8 +19,7 @@ var total_offset: Vector2 = Vector2.ZERO
 
 var progress_bar_value: float = 0.5:
 	set(new_value):
-		var clamped_value: float = minf(1.0, maxf(0, new_value))
-		progress_bar_value = clamped_value
+		progress_bar_value = clampf(new_value, 0.0, 1.0)
 		_progress_bar_value_changed(progress_bar_value)
 
 signal player_win(player_id: int)
@@ -62,6 +61,9 @@ func add_damage_to_player(player_id: int, damage: float):
 	if player_id == 0: progress_bar_with_damage -= damage / (player_health * 2)
 	elif player_id == 1: progress_bar_with_damage += damage / (player_health * 2)
 	
+	if progress_bar_with_damage <= 0: player_win.emit(get_player_id_with_most_health())
+	elif progress_bar_with_damage >= 1: player_win.emit(get_player_id_with_most_health())
+	
 	var tween: Tween = create_tween()
 	tween.tween_property(
 		self,
@@ -71,9 +73,6 @@ func add_damage_to_player(player_id: int, damage: float):
 	).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
 	
 	await tween.finished
-	
-	if progress_bar_value <= 0: player_win.emit(get_player_id_with_most_health())
-	elif progress_bar_value >= 1: player_win.emit(get_player_id_with_most_health())
 	
 	lifebar_value_change.emit(progress_bar_value)
 
