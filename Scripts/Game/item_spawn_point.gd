fb968a3a0156792b8f89ea8db1e97f0cca040ddd
Ewen R <106588588+Miit0s@users.noreply.github.com>
2026-05-09 15:28:10 +0200

    Item will no longer spawn on occupied spawn point

--- Scripts/Game/item_spawn_point.gd
+++ Scripts/Game/item_spawn_point.gd
@@ -8,6 +8,9 @@ class_name ItemSpawnPoint
 
 var is_item_currently_on_spawn: bool
 
+var _overlapping_count: int = 0
+var _will_spawn: bool = false
+
 func spawn_random_item() -> Item:
 	if possible_items.is_empty(): return
 	
@@ -18,16 +21,23 @@ func spawn_random_item() -> Item:
 
 func spawn(item: Item):
 	is_item_currently_on_spawn = true
+	_will_spawn = true
 	
 	spawn_particle.emitting = true
 	await get_tree().create_timer(spawn_delay).timeout
 	
 	add_child(item)
+	_will_spawn = false
 
 
 func _on_item_detector_body_entered(_body: Node3D) -> void:
+	_overlapping_count += 1
 	is_item_currently_on_spawn = true
 
 
 func _on_item_detector_body_exited(_body: Node3D) -> void:
-	is_item_currently_on_spawn = false
+	_overlapping_count -= 1
+	
+	if _overlapping_count <= 0 and not _will_spawn:
+		_overlapping_count = 0
+		is_item_currently_on_spawn = false
