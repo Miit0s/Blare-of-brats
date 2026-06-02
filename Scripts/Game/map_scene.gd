cbac7b0f5aa33867c961d9424021561ac652ce84
Ewen R <106588588+Miit0s@users.noreply.github.com>
2026-06-02 16:02:29 +0200

    fix: signal emit by balloon pop not register by map

--- Scripts/Game/map_scene.gd
+++ Scripts/Game/map_scene.gd
@@ -2,11 +2,15 @@ extends Node3D
 class_name MapScene
 
 @export var player_spawn_system: PlayerSpawnSystem
+@export var balloon_manager: BalloonManager
 
 signal new_player_spawn(player: Player)
 signal new_item_spawn(new_item: Item)
 signal item_will_be_delete(item: Item)
 signal balloon_pop(value: float)
+
+func _ready() -> void:
+	balloon_manager.sound_emit.connect(_on_balloon_manager_sound_emit)
 
 func _on_item_spawn_system_item_will_be_delete(item: Item) -> void:
 	item_will_be_delete.emit(item)
