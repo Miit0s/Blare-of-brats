b820af85f490f5cc924b116b9bfd991b05d9c527
Ewen R <106588588+Miit0s@users.noreply.github.com>
2026-06-08 00:01:41 +0200

    tween on move begin and end for wolf eyes
    This change updates the scene configuration and the associated script to correctly implement the wolf targeting mechanism.

--- Scripts/Game/wolf_target.gd
+++ Scripts/Game/wolf_target.gd
@@ -5,14 +5,20 @@ class_name WolfTarget
 @onready var spot_light_3d: SpotLight3D = $SpotLight3D
 
 @export var speed: float = 5
-@export var distance_for_slow_down: float = 5
+
+@export_category("Tween")
+@export var distance_for_slow_down: float = 2
 @export var slow_down_duration: float = 1
+@export var distance_for_speed_up: float = 2
+@export var speed_up_duration: float = 1
 
 var _target_position: Vector3 = Vector3.ZERO
 var _is_tracking: bool = false
 var _has_reach_destination: bool = false
+var _has_finish_restart_move: bool = true
 
 var _slow_down_tween: Tween = null
+var _speed_up_tween: Tween = null
 
 signal has_reach_destination
 
@@ -28,11 +34,15 @@ func _process(delta: float) -> void:
 		_slow_down_tween.set_ease(Tween.EASE_OUT)
 		_slow_down_tween.set_trans(Tween.TRANS_QUAD)
 		_slow_down_tween.tween_property(self, "global_position", _target_position, slow_down_duration)
-	else:
+	elif _has_finish_restart_move:
 		global_position = global_position.move_toward(_target_position, delta * speed)
 
 func set_new_target_position(new_position: Vector3):
 	_target_position = new_position
+	
+	if _has_reach_destination:
+		_restart_move()
+	
 	_has_reach_destination = false
 	
 	if _slow_down_tween:
@@ -46,3 +56,18 @@ func start_tracking():
 func stop_tracking():
 	_is_tracking = false
 	spot_light_3d.hide()
+
+func _restart_move():
+	_has_finish_restart_move = false
+	
+	var point_ratio_to_destination: float = distance_for_speed_up / global_position.distance_to(_target_position)
+	var speed_up_destination_point: Vector3 = lerp(global_position, _target_position, point_ratio_to_destination)
+	
+	_speed_up_tween = create_tween()
+	_speed_up_tween.set_ease(Tween.EASE_IN)
+	_speed_up_tween.set_trans(Tween.TRANS_QUAD)
+	_speed_up_tween.tween_property(self, "global_position", speed_up_destination_point, slow_down_duration)
+	_speed_up_tween.finished.connect(func():
+		_has_finish_restart_move = true
+		_speed_up_tween = null
+	)
