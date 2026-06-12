6ab854c13433fffa94fe7db9061ca9028ac96e22
Ewen R <106588588+Miit0s@users.noreply.github.com>
2026-06-12 16:42:45 +0200

    inte: input reminder

--- Scripts/UI/input_reminder.gd
+++ Scripts/UI/input_reminder.gd
@@ -1,14 +1,14 @@
 extends Control
 class_name InputReminder
 
-@onready var move_rich_text_label: ControllerIconParser = $ControlsContainer/ColorRect/VBoxContainer2/HBoxContainer/VBoxContainer/MoveRichTextLabel
-@onready var attack_rich_text_label_2: ControllerIconParser = $ControlsContainer/ColorRect/VBoxContainer2/HBoxContainer/VBoxContainer/AttackRichTextLabel2
-@onready var pickup_rich_text_label_3: ControllerIconParser = $ControlsContainer/ColorRect/VBoxContainer2/HBoxContainer/VBoxContainer/PickupRichTextLabel3
-@onready var dash_rich_text_label_4: ControllerIconParser = $ControlsContainer/ColorRect/VBoxContainer2/HBoxContainer/VBoxContainer/DashRichTextLabel4
-@onready var throw_rich_text_label_5: ControllerIconParser = $ControlsContainer/ColorRect/VBoxContainer2/HBoxContainer/VBoxContainer/ThrowRichTextLabel5
-
 @onready var not_ready: TextureRect = $NotReady
 @onready var ready_texture: TextureRect = $Ready
+
+@export var move_rich_text_label: ControllerIconParser
+@export var attack_rich_text_label_2: ControllerIconParser
+@export var pickup_rich_text_label_3: ControllerIconParser
+@export var dash_rich_text_label_4: ControllerIconParser
+@export var throw_rich_text_label_5: ControllerIconParser
 
 @export var player_input_id: int = 0:
 	set(new_value):
@@ -22,13 +22,6 @@ class_name InputReminder
 @export var prefix_text_dash: String
 @export var prefix_text_throw: String
 
-@export_category("Text")
-@export var text_move: String
-@export var text_attack: String
-@export var text_pickup: String
-@export var text_dash: String
-@export var text_throw: String
-
 var _is_player_ready: bool = false
 
 signal player_ready
@@ -38,11 +31,11 @@ func _ready() -> void:
 	_update_text_for_current_id()
 
 func _update_text_for_current_id():
-	move_rich_text_label.raw_text = _get_correct_prefix_for_text(prefix_text_move) + text_move
-	attack_rich_text_label_2.raw_text = _get_correct_prefix_for_text(prefix_text_attack) + text_attack
-	pickup_rich_text_label_3.raw_text = _get_correct_prefix_for_text(prefix_text_pickup) + text_pickup
-	dash_rich_text_label_4.raw_text = _get_correct_prefix_for_text(prefix_text_dash) + text_dash
-	throw_rich_text_label_5.raw_text = _get_correct_prefix_for_text(prefix_text_throw) + text_throw
+	move_rich_text_label.raw_text = _get_correct_prefix_for_text(prefix_text_move)
+	attack_rich_text_label_2.raw_text = _get_correct_prefix_for_text(prefix_text_attack)
+	pickup_rich_text_label_3.raw_text = _get_correct_prefix_for_text(prefix_text_pickup)
+	dash_rich_text_label_4.raw_text = _get_correct_prefix_for_text(prefix_text_dash)
+	throw_rich_text_label_5.raw_text = _get_correct_prefix_for_text(prefix_text_throw)
 
 func _get_correct_prefix_for_text(text: String) -> String:
 	return "{" + text + "_" + str(player_input_id) + "}"
