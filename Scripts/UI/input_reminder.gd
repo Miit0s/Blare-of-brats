40e52d50f2d984f8212ccaf72ea02d6cda5af61b
Ewen R <106588588+Miit0s@users.noreply.github.com>
2026-06-13 11:43:59 +0200

    more tween on the onboarding + input on the task
    - input reminder spawn and despawn
    - task validation
    - task list spawn

--- Scripts/UI/input_reminder.gd
+++ Scripts/UI/input_reminder.gd
@@ -23,6 +23,7 @@ class_name InputReminder
 @export var prefix_text_throw: String
 
 @export_category("Tween")
+@export var self_spawn_tween_duration: float = 0.2
 @export var ready_tween_duration: float = 0.2
 
 var _is_player_ready: bool = false
@@ -32,6 +33,7 @@ signal player_ready
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_update_text_for_current_id()
+	self.scale = Vector2.ZERO
 
 func _update_text_for_current_id():
 	move_rich_text_label.raw_text = _get_correct_prefix_for_text(prefix_text_move)
@@ -44,6 +46,8 @@ func _get_correct_prefix_for_text(text: String) -> String:
 	return "{" + text + "_" + str(player_input_id) + "}"
 
 func _input(event: InputEvent) -> void:
+	if not visible: return
+	
 	if event is InputEventJoypadButton and event.device == player_input_id:
 		if event.is_action_pressed("JoinGame"):
 			input_reminder_ready()
@@ -62,5 +66,20 @@ func input_reminder_ready():
 	ready_texture_tween.set_trans(Tween.TRANS_QUART)
 	ready_texture_tween.tween_property(ready_texture, "scale", Vector2.ONE, ready_tween_duration)
 	
+	await ready_texture_tween.finished
+	
 	_is_player_ready = true
 	player_ready.emit()
+
+func trigger_spawn_animation():
+	var self_spawn_tween: Tween = create_tween()
+	self_spawn_tween.set_ease(Tween.EASE_OUT)
+	self_spawn_tween.set_trans(Tween.TRANS_QUART)
+	self_spawn_tween.tween_property(self, "scale", Vector2.ONE, self_spawn_tween_duration)
+
+func trigger_despawn_animation():
+	var self_despawn_tween: Tween = create_tween()
+	self_despawn_tween.set_ease(Tween.EASE_IN)
+	self_despawn_tween.set_trans(Tween.TRANS_QUART)
+	self_despawn_tween.tween_property(self, "scale", Vector2.ZERO, self_spawn_tween_duration)
+	self_despawn_tween.tween_property(self, "visible", false, 0)
--- Scripts/UI/task_list.gd
+++ Scripts/UI/task_list.gd
@@ -4,12 +4,17 @@ class_name TaskList
 @onready var task_container: VBoxContainer = $TaskContainer
 @onready var texture_rect: TextureRect = $TextureRect
 
+@export var is_reverse: bool = false
 @export var task_prefab: PackedScene
+@export var spawn_tween_duration = 0.3
 
 var _task_completed: int = 0
 var _task_to_complete: int = 0
 
 signal all_task_complete
+
+signal spawn_anim_finish
+signal despawn_anim_not_finish
 
 func _ready() -> void:
 	clear_task()
@@ -17,9 +22,14 @@ func _ready() -> void:
 func setup_color_for_task_list(color: CharacterColorResource):
 	texture_rect.self_modulate = color.main_color
 
-func add_new_task(task_node: Task):
+func add_new_task(task_node: Task, task_info: TaskInfo, player_id: int):
 	task_node.task_marked_as_complete.connect(one_current_task_complete)
 	task_node.size_flags_horizontal = Control.SIZE_SHRINK_CENTER
+	
+	var copy_task_info: TaskInfo = task_info.duplicate()
+	copy_task_info.action_input = "{" + task_info.action_input + "_" + str(player_id) + "}"
+	task_node.fill_task_with_info(copy_task_info)
+	
 	task_container.add_child(task_node)
 	_task_to_complete += 1
 
@@ -33,3 +43,25 @@ func one_current_task_complete():
 	_task_completed += 1
 	
 	if _task_completed >= _task_to_complete: all_task_complete.emit()
+
+func trigger_spawn_animation():
+	var final_position: Vector2 = position
+	var begin_position: Vector2 = position + Vector2(size.x, 0) if is_reverse else position - Vector2(size.x, 0)
+	set_position(begin_position)
+	
+	var spawn_tween: Tween = create_tween()
+	spawn_tween.set_ease(Tween.EASE_OUT)
+	spawn_tween.set_trans(Tween.TRANS_QUART)
+	spawn_tween.tween_property(self, "visible", true, 0)
+	spawn_tween.tween_property(self, "position", final_position, spawn_tween_duration)
+	spawn_tween.finished.connect(spawn_anim_finish.emit)
+
+func trigger_despawn_animation():
+	var final_position: Vector2 = position - Vector2(size.x, 0) if is_reverse else position + Vector2(size.x, 0)
+	
+	var despawn_tween: Tween = create_tween()
+	despawn_tween.set_ease(Tween.EASE_IN)
+	despawn_tween.set_trans(Tween.TRANS_QUART)
+	despawn_tween.tween_property(self, "position", final_position, spawn_tween_duration)
+	despawn_tween.tween_property(self, "visible", false, 0)
+	despawn_tween.finished.connect(despawn_anim_not_finish.emit)
