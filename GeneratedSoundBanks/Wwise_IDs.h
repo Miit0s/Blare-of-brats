ed6563849a73be079f881511666de5aa311320b7
Mathisseux <mathis.dos@gmail.com>
2026-06-22 14:47:24 +0200

    [SD] New Menu Music

--- GeneratedSoundBanks/Wwise_IDs.h
+++ GeneratedSoundBanks/Wwise_IDs.h
@@ -51,11 +51,13 @@ namespace AK
 
             namespace STATE
             {
+                static const AkUniqueID MENU = 2607556080U;
                 static const AkUniqueID NONE = 748895195U;
                 static const AkUniqueID PHASE_1 = 1622949422U;
                 static const AkUniqueID PHASE_2 = 1622949421U;
                 static const AkUniqueID PHASE_3 = 1622949420U;
                 static const AkUniqueID PHASE_DANGER = 1580246308U;
+                static const AkUniqueID READYSCREEN = 505373590U;
             } // namespace STATE
         } // namespace ROUND_STATE
 
