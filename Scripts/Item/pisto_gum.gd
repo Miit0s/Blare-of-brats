05635f772bbac0abd6a015d4bdcca35b0df4e66d
Ewen R <106588588+Miit0s@users.noreply.github.com>
2026-06-24 01:32:08 +0200

    need test: the trail is now moving when shoot

--- Scripts/Item/pisto_gum.gd
+++ Scripts/Item/pisto_gum.gd
@@ -10,6 +10,8 @@ extends Item
 @export_group("On Shoot")
 @export_range(0, 1) var vibration_force_on_shoot: float = 0.8
 @export var vibration_duration_on_shoot: float = 0.1
+@export_category("Tween")
+@export var trail_effect_distance: float = 20
 
 func _perform_attack(_direction: Vector3):
 	current_durability -= 1
@@ -19,6 +21,11 @@ func _perform_attack(_direction: Vector3):
 	pistogum_trail.look_at(self.global_position - self.global_basis.z)
 	pistogum_trail.global_rotation.y += deg_to_rad(90)
 	pistogum_trail.global_rotation.x += deg_to_rad(90)
+	
+	var trail_tween: Tween = create_tween()
+	trail_tween.set_ease(Tween.EASE_OUT)
+	trail_tween.set_trans(Tween.TRANS_QUAD)
+	trail_tween.tween_property(pistogum_trail, "global_position:z", global_position.z + trail_effect_distance, attack_speed)
 	
 	pistogum_explosion.play("default")
 	pistogum_trail.play("default")
