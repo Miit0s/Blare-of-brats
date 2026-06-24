bfd76519e28b0deff59ffa7c68c5923666f16a6f
Ewen R <106588588+Miit0s@users.noreply.github.com>
2026-06-24 02:30:28 +0200

    inte: added the new onboarding pop up

--- Scripts/UI/onboarding_text_display.gd
+++ Scripts/UI/onboarding_text_display.gd
@@ -1,39 +1,47 @@
 extends Control
 class_name OnboardingTextDisplay
 
-@onready var title: Label = $VBoxContainer/Title
-@onready var text: RichTextLabel = $VBoxContainer/Text
+@onready var title: Label = $TitleBackground/Title
+@onready var text: RichTextLabel = $Text
+@onready var image_with_text: TextureRect = $ImageWithText
 
-@onready var go_back: ColorRect = $GoBack
+@onready var go_back: TextureRect = $GoBack
 
-@onready var page_number: Label = $PageNumber
+@onready var page_number: RichTextLabel = $PageNumberBackground/PageNumber
 
-var _texts: Array[String]
+var _datas: Array[OnboardingHelpBoxContent]
 
 var _current_page: int = 0
 
 signal onboarding_text_display_finish
 
-func add_text_page(new_text: String):
-	_texts.append(new_text)
+func add_text_page(new_text: OnboardingHelpBoxContent):
+	_datas.append(new_text)
 	update_page()
 	
-	if _texts.size() == 1: display_current_page()
+	if _datas.size() == 1: display_current_page()
 
-func add_texts_page(new_texts: Array[String]):
+func add_texts_page(new_texts: Array[OnboardingHelpBoxContent]):
 	for new_text in new_texts:
 		add_text_page(new_text)
 
 func display_current_page():
-	text.text = _texts[_current_page]
+	title.text = _datas[_current_page].title
+	text.text = _datas[_current_page].text
+	
+	if _datas[_current_page].image_linked:
+		image_with_text.texture = _datas[_current_page].image_linked
+	else:
+		image_with_text.texture = null
+	
 	if _current_page > 0: go_back.show()
 	elif _current_page <= 0: go_back.hide()
 
 func update_page():
-	page_number.text = str(_current_page + 1) + " / " + str(_texts.size())
+	page_number.text = str(_current_page + 1) + " / " + str(_datas.size())
 
 func next_page():
-	if _current_page >= _texts.size() - 1: 
+	if _current_page >= _datas.size() - 1: 
 		onboarding_text_display_finish.emit()
 		return
 	
@@ -52,7 +60,7 @@ func previous_page():
 
 func reset():
 	_current_page = 0
-	_texts.clear()
+	_datas.clear()
 	go_back.hide()
 
 func _input(event: InputEvent) -> void:
